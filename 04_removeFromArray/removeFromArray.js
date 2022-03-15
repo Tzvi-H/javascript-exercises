62bca31c02864a7e4f79c39d701cd85ab2a8822a
Tzvi-H <16guitar@gmail.com>
2022-03-15 13:21:22 -0500

    completed exercises 4-7

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,5 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+  return array.filter(num => args.indexOf(num) === -1)
 };
 
 // Do not edit below this line
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+  return (year % 400 === 0) ||
+         (year % 4 === 0 && year % 100 !== 0);
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+
+/*
+- divisible by 4
+- not divisible by 100
+- unless divisible by 400
+
+*/
