ce04a166b26b6686fec2840224b16ab0e7acad11
Tzvi-H <16guitar@gmail.com>
2022-03-17 13:51:36 -0500

    completed 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,26 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  let left = 0;
+  let right = string.length - 1;
+  while (left < right) {
+    while (left < right && !isALetter(string[left])) {
+      left++
+    }
+    while (left < right && !isALetter(string[right])) {
+      right--
+    }
+    if (string[left].toLowerCase() !== string[right].toLowerCase()) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+  return true;
 };
+
+const isALetter = x => {
+  const code = x.toLowerCase().charCodeAt();
+  return code >= 97 && code <= 122;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
