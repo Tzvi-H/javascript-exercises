2b6ecbd59cd927f0cebc4ee8cdcae6b6fe901c32
Tzvi-H <16guitar@gmail.com>
2022-03-17 14:08:50 -0500

    completed 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  return people.sort((a, b) => {
+    const ageA = (a.yearOfDeath || new Date().getFullYear()) - a.yearOfBirth;
+    const ageB = (b.yearOfDeath || new Date().getFullYear()) - b.yearOfBirth;
+    return ageB - ageA;
+  })[0];
 };
 
 // Do not edit below this line
