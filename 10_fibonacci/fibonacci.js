e67b179d7c9dc3b9742c939e31c7a812f6d9ee28
Tzvi-H <16guitar@gmail.com>
2022-03-17 13:56:31 -0500

    completed 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(x) {
+  x = parseInt(x);
+  if (x < 0) {
+    return 'OOPS';
+  }
 
+  let a = 1;
+  let b = 1;
+  let count = 2;
+
+  while (count < x) {
+    [ b, a ] = [ a + b, b ]
+    count++
+  }
+
+  return b;
 };
 
 // Do not edit below this line
